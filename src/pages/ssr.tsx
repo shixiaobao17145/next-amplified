a723d24f6a9933ab3b5dd315f98a92dc9d748e4e
Bob <xiaobshi@cisco.com>
2023-02-22 16:23:27 +0800

    add static page

--- src/pages/ssr.tsx
+++ src/pages/ssr.tsx
@@ -1,26 +1,27 @@
-export default function SSR({ formattedDate }) {
-    return (
-      <>
-        <h1>Server-side rendered page</h1>
-        <p>
-          This page is server-side rendered. It was rendered on {formattedDate}.
-        </p>
-        <p>
-          <a href="/">View a static page.</a>
-        </p>
-      </>
-    );
-  }
-  
-  export async function getServerSideProps() {
-    const renderDate = Date.now();
-    const formattedDate = new Intl.DateTimeFormat("en-US", {
-      dateStyle: "long",
-      timeStyle: "long",
-    }).format(renderDate);
-    console.log(
-      `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
-    );
-    return { props: { formattedDate } };
-  }
-  
+import Link from "next/link";
+
+export default function SSR({ formattedDate }: { formattedDate: any }) {
+  return (
+    <>
+      <h1>Server-side rendered page</h1>
+      <p>
+        This page is server-side rendered. It was rendered on {formattedDate}.
+      </p>
+      <p>
+        <Link href="/">View a static page.</Link>
+      </p>
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const renderDate = Date.now();
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "long",
+    timeStyle: "long",
+  }).format(renderDate);
+  console.log(
+    `SSR ran on ${formattedDate}. This will be logged in CloudWatch.`
+  );
+  return { props: { formattedDate } };
+}
